47c0280979964bcb8c29357c5d07bf8f14dfc329
Luciano Teixeira <luciano.teixeiras@gmail.com>
2018-04-20 17:00:36 -0300

    Commit add Calcula Salário. Signed-off-by:Luciano Teixeira <luciano.teixeiras@gmail.com>

---
+++ BecodeJavaIniciantes/src/CalculaSalario/CalculaSalario.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CalculaSalario;
+
+/**
+ *
+ * @author luciano.t.da.silva
+ */
+public class CalculaSalario {
+    
+    public static void main(String[] args) {
+        
+        Inicial FrmInicial = new Inicial();
+        FrmInicial.setVisible(true);
+        
+    }
+    
+}
